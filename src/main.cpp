4ce3f8d9ebffce0c931a780988ffb954a5a81e1a
Karthick <karthickps.ai@gmail.com>
2020-09-01 04:51:01 +0530

    Added OpenCV Cap

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 #include <gst/pbutils/pbutils.h>
 
 int main() {
@@ -11,6 +13,52 @@ int main() {
     std::cout << "OpenCV Version " << CV_VERSION << std::endl;
     std::cout << "gStreamer Version " << GST_VERSION_MAJOR << "." << GST_VERSION_MINOR << "." << GST_VERSION_MICRO
               << std::endl;
+<<<<<<< b065d62fb7e979222ec41930dcbd99d426d01dc0
     std::cout << "Hello World" << std::endl;
+=======
+    tensorflow::DeviceTypeString("GPU");
+    tensorflow::Session *session;
+    tensorflow::Status status = tensorflow::NewSession(tensorflow::SessionOptions(), &session);
+    if (!status.ok()) {
+        std::cout << status.ToString() << "\n";
+        return 1;
+    }
+
+    // Load the protobuf graph
+    tensorflow::GraphDef graph_def;
+    std::string graph_path = "../bin/mobel.pb";
+    status = tensorflow::ReadBinaryProto(tensorflow::Env::Default(), graph_path, &graph_def);
+    if (!status.ok()) {
+        std::cerr << status.ToString() << std::endl;
+        return 1;
+    } else {
+        std::cout << "Load graph protobuf successfully" << std::endl;
+    }
+
+    // Add the graph to the session
+    status = session->Create(graph_def);
+    if (!status.ok()) {
+        std::cerr << status.ToString() << std::endl;
+        return 1;
+    } else {
+        std::cout << "Add graph to session successfully" << std::endl;
+    }
+
+    session->Close();
+
+    static const std::string windowName = "Virtual WebCam";
+    cv::namedWindow(windowName, cv::WINDOW_NORMAL);
+    cv::VideoCapture cap;
+    cap.open(std::string("/dev/video0"));
+    cv::Mat frame, blob;
+    while (cv::waitKey(1) < 0) {
+        cap >> frame;
+        if (frame.empty()) {
+            cv::waitKey();
+            break;
+        }
+        cv::imshow(windowName, frame);
+    }
+>>>>>>> Added OpenCV Cap
     return 0;
 }

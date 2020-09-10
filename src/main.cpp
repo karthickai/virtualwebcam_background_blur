2e73f6e18cd5dc15367dd7b941433807f1653658
Karthick <karthickps.ai@gmail.com>
2020-09-10 23:01:12 +0530

    Model ops list

--- src/main.cpp
+++ src/main.cpp
@@ -12,24 +12,36 @@ int main() {
     std::cout << "OpenCV Version " << CV_VERSION << std::endl;
     std::cout << "TensorFlow library version " << TF_Version() << std::endl;
 
-    Model model("../bin/mobel.pb");
-    Tensor outNames1{model, "resnet_v1_50/displacement_bwd_2/BiasAdd:0"};
-    Tensor inpName{model, "sub_2:0"};
+
 
     // Read image
     cv::Mat img, inp;
-    img = cv::imread("../test.png", cv::IMREAD_COLOR);
+    img = cv::imread("../bin/simple.jpg", cv::IMREAD_COLOR);
 
     int rows = img.rows;
     int cols = img.cols;
 
-    cv::resize(img, inp, cv::Size(300, 300));
+    auto frame_width = img.size().width;
+    auto frame_height = img.size().height;
+    int output_stride = 16;
+    auto target_width = std::floor(int(frame_width) / output_stride) * output_stride + 1;
+    auto target_height = std::floor(int(frame_height) / output_stride) * output_stride + 1;
+    cv::resize(img, inp, cv::Size(target_width, target_height));
     cv::cvtColor(inp, inp, cv::COLOR_BGR2RGB);
+
+    std::cout << inp.size << std::endl;
 
     // Put image in Tensor
     std::vector<uint8_t > img_data;
     img_data.assign(inp.data, inp.data + inp.total() * inp.channels());
-    inpName.set_data(img_data, {1, 300, 300, 3});
+    Model model("../bin/mobel_old.pb");
+    for (auto ops : model.get_operations()){
+        std::cout << ops << std::endl;
+    }
+    Tensor outNames1{model, "float_heatmaps"};
+    Tensor inpName{model, "sub_2"};
+
+    inpName.set_data(img_data, {1, static_cast<long>(target_width), static_cast<long>(target_height), 3});
 
     model.run(inpName, {&outNames1});
 
